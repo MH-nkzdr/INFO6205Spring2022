d156e36472e098666cfe40d05ee6ec48983d14c5
minxi <manzikhon@gmail.com>
2022-02-24 16:38:12 -0500

    INFO6205 Assignment2

--- assignment2/src/SearchingAndSorting.java
+++ assignment2/src/SearchingAndSorting.java
@@ -220,49 +220,28 @@ public class SearchingAndSorting {
 	// 18. 4Sum
 	public static List<List<Integer>> fourSum(int[] nums, int target) {
 		List<List<Integer>> ret = new ArrayList<>();
-		if (nums == null || nums.length < 4) {
-			return ret;
-		}
 		Arrays.sort(nums);
-		int length = nums.length;
-		for (int i = 0; i < length - 3; i++) {
+		for (int i = 0; i < nums.length; i++) {
 			if (i > 0 && nums[i] == nums[i - 1]) {
 				continue;
 			}
-			if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
-				break;
-			}
-			if (nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
-				continue;
-			}
-			for (int j = i + 1; j < length - 2; j++) {
+			for (int j = i + 1; j < nums.length; j++) {
 				if (j > i + 1 && nums[j] == nums[j - 1]) {
 					continue;
 				}
-				if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
-					break;
-				}
-				if (nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
-					continue;
-				}
-				int lo = j + 1;
-				int hi = length - 1;
-				while (lo < hi) {
-					int sum = nums[i] + nums[j] + nums[lo] + nums[hi];
-					if (sum == target) {
-						ret.add(Arrays.asList(nums[i], nums[j], nums[lo], nums[hi]));
-						lo++;
-						while (lo < hi && nums[lo] == nums[lo - 1]) {
-							lo++;
-						}
-						hi--;
-						while (lo < hi && nums[hi] == nums[hi + 1]) {
-							hi--;
-						}
-					} else if (sum < target) {
-						lo++;
+				int left = j + 1;
+				int right = nums.length - 1;
+				while (left < right) {
+					if (nums[i] + nums[j] + nums[left] + nums[right] > target) {
+						right--;
+					} else if (nums[i] + nums[j] + nums[left] + nums[right] < target) {
+						left++;
 					} else {
-						hi--;
+						ret.add(List.of(nums[i], nums[j], nums[left], nums[right]));
+						while (left < right && nums[left] == nums[left + 1]) left++;
+						while (left < right && nums[right] == nums[right - 1]) right--;
+						left++;
+						right--;
 					}
 				}
 			}
